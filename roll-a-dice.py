1406fcf62d6412574c1658aebf181ebe4f5cb88a
manashvi07032006 <80456520+manashvi07032006@users.noreply.github.com>
2024-05-24 14:32:46 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,47 @@
+import random
+
+
+
+def randint(start, end):
+    response = "y"
+    while response=="y":
+        no = int(random.randint(start, end))
+        if (no==1):
+            print("[---]")
+            print("[   ]")
+            print("[ 0 ]")
+            print("[   ]")
+            print("[---]")
+        elif (no==2):
+            print("[---]")
+            print("[0  ]")
+            print("[   ]")
+            print("[  0]")
+            print("[---]")
+        elif (no==3):
+            print("[---]")
+            print("[0  ]")
+            print("[ 0 ]")
+            print("[  0]")
+            print("[---]")
+        elif (no==4):
+            print("[---]")
+            print("[0 0]")
+            print("[   ]")
+            print("[0 0]")
+            print("[---]")
+        elif (no==5):
+            print("[---]")
+            print("[0 0]")
+            print("[ 0 ]")
+            print("[0 0]")
+            print("[---]")
+        elif (no==6):     
+            print("[---]")
+            print("[0 0]")
+            print("[0 0]")
+            print("[0 0]")
+            print("[---]")
+        response = input("Press y to roll again and n to exit:  ")
+
+randint(1,6)
